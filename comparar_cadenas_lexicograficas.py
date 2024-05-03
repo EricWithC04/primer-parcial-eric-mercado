757016533efd28d96ed5455e9b21c2852f9d9c41
EricWithC04 <negativegame117@gmail.com>
2024-05-03 10:41:13 -0300

    feat: add comments in comparar_cadenas_lexicograficas

--- comparar_cadenas_lexicograficas.py
+++ comparar_cadenas_lexicograficas.py
@@ -1,8 +1,10 @@
 def comparar_cadenas_lexicograficas(cadena1, cadena2):
     
+    # Con el metodo isalpha, comprobamos que cada uno de los caracteres sea una letra
     letras1 = [c.isalpha() for c in cadena1]
     letras2 = [c.isalpha() for c in cadena2]
     
+    # Validaciones, tipo de dato, longitud, que los caracteres sean exclusivamente letras, que estén en mayusculas y ambas cadenas tengan la misma longitud
     if (type(cadena1) != str or type(cadena2) != str):
         raise TypeError("Ambos parametros deben ser cadenas!")
     
@@ -18,17 +20,20 @@ def comparar_cadenas_lexicograficas(cadena1, cadena2):
     if (len(cadena1) != len(cadena2)):
         raise ValueError("Ambas cadenas deben tener la misma longitud")
     
+    # Obtenemos el largo de las cadenas
     if len(cadena1) > len(cadena2):
         longitud_cadena = len(cadena1)
     else:
         longitud_cadena = len(cadena2)
     
+    # Recorremos y comparamos cada uno de los caracteres de ambas cadenas
     for i in range(longitud_cadena):
         if (cadena1[i] < cadena2[i]):
             return "-1"
         elif (cadena2[i] < cadena1[i]):
             return "1"
     
+    # Si la función no retorno nada dentro del for, significa que ambas cadenas son iguales, retornamos "0"
     return "0"
 
 print(comparar_cadenas_lexicograficas("HOLA", "ABCD"))
