51e45db31cf76bf26c3e9db43e35446ee1fdda78
EricWithC04 <negativegame117@gmail.com>
2024-05-03 12:24:05 -0300

    feat: comments in graficos.py

--- graficos.py
+++ graficos.py
@@ -1,11 +1,19 @@
 import matplotlib.pyplot as plt
 from ejercicio_pandas import retorno_dataframe as rd
 
+# Traemos el DataFrame del anterior ejercicio
 v_m = rd()
 
+# Utilizamos sus datos para crear un gráfico de barras
 plt.plot(v_m["mes"], v_m["total_ventas"], color="blue")
+
+# Colocamos el titulo y los nombres de los ejes
 plt.title("Tendencia de ventas")
 plt.xlabel("Meses")
 plt.ylabel("Ventas")
+
+# Agregamos una cuadricula en el fondo
 plt.grid(True)
+
+# Mostramos el gráfico
 plt.show()
